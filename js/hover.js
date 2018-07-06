fd425dc1cc69e8b9c559069e3ab1a7117a488212
Nate Letz <marionettis2@gmail.com>
2018-07-05 21:38:19 -0700

    New Website

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function hoverVanilla(element) {
+	element.setAttribute('src', 'img/vanilla-color.png')
+}
+
+function unhoverVanilla(element) {
+	element.setAttribute('src', 'img/vanilla-grayscale.png')
+}
